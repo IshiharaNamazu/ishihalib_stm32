6ef58e1aed7c49b401aa9c0d395f47605baac961
Takuto Ishihara <ishikorotakkun@gmail.com>
2021-11-20 16:24:21 +0900

    二乗平均追加

--- utility/math_util.hpp
+++ utility/math_util.hpp
@@ -28,4 +28,31 @@ static inline double normalize_Deg(double deg) {  //-180<deg<=180に直す
 	while (deg > 180.) deg -= 2 * 180;
 	return deg;
 }
+
+class MeanSquared{//二乗平均
+	private:
+	const unsigned int max_deta_number;
+	unsigned int data_num;
+	double sum;
+	public:
+	MeanSquared(const int max_data_num = __UINT32_MAX__/2) : max_deta_number(max_data_num), data_num(0), sum(0){
+	}
+
+	double calc(double data){
+		if((data_num+1)>max_deta_number)return -1;
+		data_num++;
+		sum+=data*data;
+		return sum/data_num;
+	}
+
+	double get(){
+		if(data_num<=0) return -1;
+		return sum/data_num;
+	}
+
+	void reset(){
+		data_num=0;
+		sum=0;
+	}
+};
 };	// namespace ishihalib
