484754d1c700da1439a3b33a0eac9f992057f9e4
Takuto Ishihara <ishikorotakkun@gmail.com>
2022-01-18 22:31:24 +0900

    Ticker追加

--- hal_driver/hal_driver.hpp
+++ hal_driver/hal_driver.hpp
@@ -1,3 +1,4 @@
 #pragma once
 #include "adc_dma.hpp"
 #include "digitalIO.hpp"
+#include "ticker.hpp"
---
+++ hal_driver/ticker.hpp
@@ -0,0 +1,46 @@
+#pragma once
+//#ifndef HAL_TIM_MODULE_ENABLED
+#include <functional>
+#include <map>
+#include <utility>
+
+#include "tim.h"
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim);
+namespace ishihalib {
+class Ticker {
+  private:
+	TIM_HandleTypeDef *handle_;
+	const long callinterval_;																		//割り込みが呼ばれるインターバル
+	const long mx_counter_period_;																	//cubeideで設定したカウンター
+	long time_counter_;																				//経過時間
+	std::array<std::vector<std::pair<std::function<void(void)>, long>>, 4> callback_func_;			//callback_func_[priority][index]
+	inline static std::map<TIM_HandleTypeDef *, std::function<void(void)>> timer_callbackcallback;	//コールバックのコールバック
+
+  public:
+	Ticker(TIM_HandleTypeDef *handle, const long callinterval = 1000, const long mx_counter_period = 1000)
+		: handle_(handle), callinterval_(callinterval), mx_counter_period_(mx_counter_period), time_counter_(0) {
+		HAL_TIM_Base_Stop_IT(handle_);
+		timer_callbackcallback.insert(std::make_pair(handle, [&]() {
+			time_counter_ += callinterval_;
+			for (int priority = callback_func_.size() - 1; priority >= 0; priority--)
+				for (auto &f : callback_func_[priority])
+					if (!(time_counter_ % f.second)) (f.first)();
+		}));
+		HAL_TIM_Base_Start_IT(handle);
+	}
+
+	bool add_callback(std::function<void(void)> fnc, long interval_us = 1, uint8_t priority = 0) {	//priority 0:low 3:high 4:extreme
+		callback_func_[priority].push_back({fnc, interval_us});
+	}
+
+	static void timers_callback(TIM_HandleTypeDef *htim) {	//HAL_TIM_PeriodElapsedCallbackから呼ばれるための関数
+		(timer_callbackcallback[htim])();
+	}
+};
+}  // namespace ishihalib
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	ishihalib::Ticker::timers_callback(htim);
+}
+
+//#endif
