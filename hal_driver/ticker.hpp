4487be7fdd7b378321bb8e396807512c67d1eb36
Takuto Ishihara <ishikorotakkun@gmail.com>
2022-01-22 16:16:34 +0900

    インクルードガード

--- hal_driver/ticker.hpp
+++ hal_driver/ticker.hpp
@@ -1,5 +1,5 @@
 #pragma once
-//#ifndef HAL_TIM_MODULE_ENABLED
+#ifdef HAL_TIM_MODULE_ENABLED
 #include <functional>
 #include <map>
 #include <utility>
@@ -12,7 +12,7 @@ class Ticker {
 	TIM_HandleTypeDef *handle_;
 	const long callinterval_;																		//割り込みが呼ばれるインターバル
 	const long mx_counter_period_;																	//cubeideで設定したカウンター
-	long time_counter_;																				//経過時間
+	long long time_counter_;																		//経過時間
 	std::array<std::vector<std::pair<std::function<void(void)>, long>>, 4> callback_func_;			//callback_func_[priority][index]
 	inline static std::map<TIM_HandleTypeDef *, std::function<void(void)>> timer_callbackcallback;	//コールバックのコールバック
 
@@ -29,7 +29,7 @@ class Ticker {
 		HAL_TIM_Base_Start_IT(handle);
 	}
 
-	bool add_callback(std::function<void(void)> fnc, long interval_us = 1, uint8_t priority = 0) {	//priority 0:low 3:high 4:extreme
+	void add_callback(std::function<void(void)> fnc, long interval_us = 1, uint8_t priority = 0) {	//priority 0:low 3:high 4:extreme
 		callback_func_[priority].push_back({fnc, interval_us});
 	}
 
@@ -43,4 +43,4 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 	ishihalib::Ticker::timers_callback(htim);
 }
 
-//#endif
+#endif
